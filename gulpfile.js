5edf7a28aa3deb6a80ffae6afcc84e9c17f0455f
vannoel <vannoel0628@gmail.com>
2019-10-05 18:43:57 +0800

    Update page

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+const path_libs = './libs'
+const path_homepage = '../blacktoolboxlaboratory.github.io/';
+
+gulp.task('copyVendor', function(done) { 
+  /* jQuery */
+  gulp.src(['node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest(path_libs+'/jquery/'));
+
+  /* fontawesome */
+  gulp.src(['node_modules/@fortawesome/fontawesome-free/css/**/*'])
+    .pipe(gulp.dest(path_libs+'/fontawesome/css'));
+  gulp.src(['node_modules/@fortawesome/fontawesome-free/webfonts/**/*'])
+    .pipe(gulp.dest(path_libs+'/fontawesome/webfonts'));
+  
+  done();
+});
+
+gulp.task('updateHomePage', function(done){
+  /* html & ico*/
+  gulp.src(['index.html', 'favicon.ico'])
+    .pipe(gulp.dest(path_homepage));
+
+  /* style */
+  gulp.src(['style/index.css'])
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest(path_homepage+'/style'))
+  
+  /* font */
+  gulp.src(['style/google-fonts/*'])
+    .pipe(gulp.dest(path_homepage+'/style/google-fonts'))
+
+  /* libs */
+  gulp.src(['libs/**/*'])
+    .pipe(gulp.dest(path_homepage+'/libs/'))
+  
+  /* img */
+  gulp.src(['img/**/*'])
+    .pipe(gulp.dest(path_homepage+'/img/'))
+
+  done();
+});
